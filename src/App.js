ca72aeaf9c4f1520f4eb553384b8170555c8db3f
kingtrip3 <71353638+kingtrip3@users.noreply.github.com>
2021-08-22 17:12:38 -0400

    working code

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      data: false,
+    };
+  }
 
-constructor() {
-  super();
-
-  this.state = {
-    data:false
-  };
-}
-
-componentDidMount() {
-  let url = 'https://api.hatchways.io/assessment/sentences/1';
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).then((result) => {
-    // result.json().then((resp)=>{
-
-    //   this.setState({data:resp})
-    // })
-    result.json().then((resp) => {
-      // resp is: {"data":{"sentence":"I love learning code"}}
-      const { data } = resp
-      this.setState({ data })
-    })
-  })
-
-}
+  componentDidMount() {
+    let url = "https://api.hatchways.io/assessment/sentences/1";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((result) => {
+      result.json().then((resp) => {
+        this.setState({ data: resp });
+      });
+    });
+  }
 
   render() {
-    const data=this.state.data;
-    const letters = Array.from(data.sentence)
-    console.warn('data');
+    const data = this.state.data;
+
+    console.warn("data");
     return (
-     <div>
-       {
-        data ?
-         <div>
-           <h1 className="title">{letters}</h1>
-         </div>
-         : <h1>Please wait...</h1>
-       }
-     </div>
-  )}
-  
+      <div>
+        {data ? (
+          <div>
+            <h1 className="title">{data.data.sentence}</h1>
+           
+          </div>
+        ) : (
+          <h1>Please wait...</h1>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
-
